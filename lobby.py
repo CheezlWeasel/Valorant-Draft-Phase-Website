2b32c1396a86b50a965de4390cd705ae0648dcd9
CheezlWeasel <114047172+CheezlWeasel@users.noreply.github.com>
2023-08-18 00:31:37 +0000

    Updated create_code to not be reliant on an empty string in active codes, added template renders to server.py

--- lobby.py
+++ lobby.py
@@ -5,48 +5,48 @@ active_codes = ['']
 
 
 def create_code(length, active_codes):
-    code = ''
-    characters = string.ascii_uppercase + string.digits
-    print(random.choice(characters))
-    while code in active_codes:
-        code = ''.join(random.choice(characters) for i in range(length))
-    active_codes.append(code)
-    return code
+  characters = string.ascii_uppercase + string.digits
+  code = ''.join(random.choice(characters) for i in range(length))
+  print(random.choice(characters))
+  while code in active_codes:
+    code = ''.join(random.choice(characters) for i in range(length))
+  active_codes.append(code)
+  return code
 
 
 class Lobby:
-    def __init__(self, admin, name, draft_type, map_type):
-        global active_codes
-        self.name = name #string
-        self.code = create_code(4, active_codes)
-        self.players = []
-        self.admins = [admin]
-        self.captains = []
-        self.team_names = ['',''] #team 1 is attack, team 2 is defence
-        self.agent_history = []
-        self.map_history = []
-        self.agent_draft = draft_type #Boolean
-        self.map_type = map_type #integer
-        #map types are as follows, 
-        #0 = Admin picks map Bo1
-        #1 = Players pick map Bo1
-        #2 = Players pick map Bo3
-        #3 = Players pick map Bo5
 
-    def undo_agent(self):
-        if self.agent_history[0] != 0:
-            del self.agent_history[-1]
-        else:
-            print('undo failed')
-        if len(self.agent_history) == 0:
-            self.agent_history = [0]
-          
-    def undo_map(self):
-        if self.map_history[0] != 0:
-            del self.map_history[-1]
-        else:
-            print('undo failed')
-        if len(self.map_history) == 0:
-            self.map_history = [0]
+  def __init__(self, admin, name, draft_type, map_type):
+    global active_codes
+    self.name = name  #string
+    self.code = create_code(4, active_codes)
+    self.players = []
+    self.admins = [admin]
+    self.captains = []
+    self.team_names = ['', '']  #team 1 is attack, team 2 is defence
+    self.agent_history = []
+    self.map_history = []
+    self.agent_draft = draft_type  #Boolean
+    self.map_type = map_type  #integer
+    self.all_maps = True
+    #map types are as follows,
+    #0 = Admin picks map Bo1
+    #1 = Players pick map Bo1
+    #2 = Players pick map Bo3
+    #3 = Players pick map Bo5
 
-      
+  def undo_agent(self):
+    if self.agent_history[0] != 0:
+      del self.agent_history[-1]
+    else:
+      print('undo failed')
+    if len(self.agent_history) == 0:
+      self.agent_history = [0]
+
+  def undo_map(self):
+    if self.map_history[0] != 0:
+      del self.map_history[-1]
+    else:
+      print('undo failed')
+    if len(self.map_history) == 0:
+      self.map_history = [0]
--- server.py
+++ server.py
@@ -10,18 +10,22 @@ socketio = SocketIO(app, async_mode="eventlet")
 def main():
     return render_template("[PLACEHOLDER]")
 
-@app.route("/login")
-def login():
+@app.route("/create")
+def create():
     return render_template("[PLACEHOLDER]")
 
-  
-@socketio.on('create_lobby')
+
+
+@socketio.on('/create_lobby')
 def create_lobby():
     lobbies.append(Lobby(session['id']))
     socketio.emit('updated_lobbies', lobbies, broadcast=True)
 
-
-
+for i in range(len(lobbies)):
+  @socketio.on(f'/{lobbies[i].code}')
+  def lobby():
+      return render_template("[PLACEHOLDER]", lobby = lobbies[i])
+  
 if __name__ == '__server__':
     socketio.run(app, host='0.0.0.0', port='80', debug=False)
 #server communication will be run out of this file
